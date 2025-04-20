c1c06505106d5437c9124e88bbf74e759c09d62f
Glantucan <glantucan@gmail.com>
2025-04-20 23:08:13 +0200

    Home totalmente en español con introduccion básica

--- components/Home/Home.jsx
+++ components/Home/Home.jsx
@@ -9,17 +9,9 @@ const Home = ({ posts }) => {
   return (
     <StyledHome>
       <Title style={{ textAlign: 'center' }}>
-        Welcome to Glantucan's <br />
-        Photography Blog
+        El blog de NewbeForever/Glantucan <br />
       </Title>
-      <Paragraph>
-        This is a simple, elegant blog dedicated to showcasing beautiful photography. Browse through
-        our collections and enjoy the visual journey.
-      </Paragraph>
-      <Paragraph>
-        Our minimalist design puts the focus on the images, allowing the photography to speak for
-        itself. Each post features high-quality images with the stories behind them.
-      </Paragraph>
+      <Paragraph>Aprendiendo a desaprender.. y otras fricadas varias.</Paragraph>
 
       {posts.length > 0 ? (
         <ul>
@@ -27,24 +19,24 @@ const Home = ({ posts }) => {
             <li key={post.slug}>
               <h2>{post.title}</h2>
               <p>{new Date(post.date).toLocaleDateString()}</p>
-              <p>Category: {post.category}</p> {/* Display category */}
-              <p>Tags: {post.tags.join(', ')}</p> {/* Display tags */}
+              <p>Categoría: {post.category}</p> {/* Display category */}
+              <p>Etiquetas: {post.tags.join(', ')}</p> {/* Display tags */}
               {post.featuredImages &&
                 post.featuredImages.map((fImage, index) => (
                   <Image
                     key={fImage}
                     width={200}
                     src={fImage}
-                    alt={`Featured Image ${index + 1}`}
+                    alt={`Imagen destacada ${index + 1}`}
                   />
                 ))}
               <p>{post.excerpt}</p>
-              <p>{post.readingTime} min read</p>
+              <p>Lectura de {post.readingTime} min</p>
             </li>
           ))}
         </ul>
       ) : (
-        <p>No posts found.</p>
+        <p>No se han encontrado publicaciones.</p>
       )}
     </StyledHome>
   );
